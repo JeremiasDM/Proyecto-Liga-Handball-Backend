6915eaac1552c70b9720ed18eeb10089a4c12602
FacuGallardo <166397923+FacuGallardo@users.noreply.github.com>
2025-08-28 19:33:37 -0300

    Add files via upload

---
+++ FrontEnd/src/Fixture/PartidoItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import type { Encuentro } from "../types/types";
+
+type Props = { partido: Encuentro };
+
+const PartidoItem: React.FC<Props> = ({ partido }) => (
+  <li style={{ marginBottom: 4 }}>
+    <strong>J{partido.jornada}</strong> | Grupo {partido.grupo} | {partido.club1} vs {partido.club2} <span style={{ color: "#1F3C88" }}>({partido.resultado})</span>
+  </li>
+);
+
+export default PartidoItem;
